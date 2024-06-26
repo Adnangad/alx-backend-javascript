16a9933097280fc4a7a7e500bfb17952b5778d9f
Adnangad <gardobuyaadnan@gmail.com>
2024-06-26 04:50:37 -0700

    fixed lint issues

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		let ans = true;
-		if (ans === true) {
-			resolve('true');
-		}
-		else {
-			reject('flase');
-		};
-	})
+  return new Promise((resolve, reject) => {
+    const ans = true;
+    if (ans === true) {
+      resolve('true');
+    } else {
+      reject(new Error('flase'));
+    }
+  });
 }
