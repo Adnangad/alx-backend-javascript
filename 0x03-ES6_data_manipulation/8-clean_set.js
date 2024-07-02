8ad770113c80ae99312633112c386046fadc9d29
Adnangad <gardobuyaadnan@gmail.com>
2024-07-02 10:45:10 -0700

    4-filterand map used together

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function hasValuesFromArray(set, str) {
-  let finalString = '';
+  if (typeof str !== 'string' || str.length === 0) {
+    return '';
+  }
+  let finalString = [];
   set.forEach((value) => {
     if (value.startsWith(str) === true) {
-      let temp = value;
-      temp = temp.replace(/bon/gi, '-');
-      finalString += temp;
+      finalString.push(value.slice(str.length);
     }
   });
-  finalString = finalString.slice(1);
-  return finalString;
+  return finalString.join('-');
 }
