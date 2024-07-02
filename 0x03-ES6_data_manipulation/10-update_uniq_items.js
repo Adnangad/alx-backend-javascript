a525a5612f1f5a01575c35f484e829075eae73d5
Adnangad <gardobuyaadnan@gmail.com>
2024-07-02 07:14:34 -0700

    10-fixed error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,12 @@
 export default function updateUniqueItems(map) {
-  map.forEach((value, key) => {
-    if (value === 1) {
-      map.set(key, 100);
-    }
-  });
-  return map;
+  try {
+    map.forEach((value, key) => {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    });
+    return map;
+  } catch (error) {
+    throw new Error('Cannot process');
+  }
 }
