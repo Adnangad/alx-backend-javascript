293aaf5c4d2e19ac97a973f4641dba845dd9b1af
Adnangad <gardobuyaadnan@gmail.com>
2024-08-19 19:39:08 +0300

    1-stdin fixed errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+  const data = process.stdin.read();
 
-  if (chunk) {
+  if (data) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
