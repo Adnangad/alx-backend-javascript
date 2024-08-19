7837ee85ec4be7d855ea5187911ff929b89b2095
Adnangad <gardobuyaadnan@gmail.com>
2024-08-19 19:28:22 +0300

    2-reading files

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,10 @@
-const displayMessage = (messag) => {
-    console.log(messag);
-}
-module.exports = displayMessage;
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
