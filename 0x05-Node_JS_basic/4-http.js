0b16699943154dfb74d4fa2f8447dc5d28689e7a
Adnangad <gardobuyaadnan@gmail.com>
2024-08-20 13:41:32 +0300

    5-displaying file contents of a file as a response

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,21 +1,21 @@
-const http = require("http"),
-    app = http.createServer((req, resp) => {
-        try {
-            resp.statusCode = 200;
-            resp.setHeader(
-                "Content-Type",
-                "text/plain"
-            );
-            resp.end("Hello Holberton School!");
-        } catch (error) {
-            throw new Error(error);
-        }
-    }),
-    PORT = 1245;
+const http = require('http');
+const app = http.createServer((req, resp) => {
+  try {
+    resp.statusCode = 200;
+    resp.setHeader(
+      'Content-Type',
+      'text/plain'
+    );
+    resp.end('Hello Holberton School!');
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const PORT = 1245;
 app.listen(
-    PORT,
-    () => {
-        console.log("...");
-    }
+  PORT,
+  () => {
+    console.log('...');
+  }
 );
 module.exports = app;
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = 1245;
+
+const app = http.createServer((req, resp) => {
+  const { method, url } = req;
+  if (url === '/' && method === 'GET') {
+    resp.writeHead(200, { 'Content-Type': 'text/plain' });
+    resp.end('Hello Holberton School!');
+  } else if (url === '/students' && method === 'GET') {
+    resp.writeHead(200, { 'Content-Type': 'text/plain' });
+    resp.write('This is the list of our students\n');
+    const readStream = fs.createReadStream('database.csv', 'utf-8');
+
+    let data = '';
+    readStream.on('data', chunk => {
+      data += chunk; // Accumulate chunks of data
+    });
+
+    readStream.on('end', () => {
+      const fileLines = data.toString('utf-8').trim().split('\n');
+      const propNames = fileLines[0].split(',');
+      const finalList = [];
+
+      for (let i = 1; i < fileLines.length; i++) {
+        const valueList = fileLines[i].split(',');
+        const obj = {};
+        propNames.forEach((value, index) => {
+          obj[value] = valueList[index];
+        });
+        finalList.push(obj);
+      }
+
+      const sw = [];
+      const cs = [];
+      finalList.forEach((element) => {
+        if (element.field === 'SWE') {
+          sw.push(element.firstname);
+        } else if (element.field === 'CS') {
+          cs.push(element.firstname);
+        }
+      });
+
+      const stdObj = { CS: cs, SWE: sw };
+
+      resp.write(`Number of students: ${finalList.length}\n`);
+      for (const [field, students] of Object.entries(stdObj)) {
+        resp.write(
+          `Number of students in ${field}: ${
+            students.length
+          }. List: ${students.join(', ')}\n`
+        );
+      }
+      resp.end();
+    });
+
+    readStream.on('error', (err) => {
+      resp.end(`Error: ${err.message}`);
+    });
+  } else {
+    resp.writeHead(404, { 'Content-Type': 'text/plain' });
+    resp.end('Not Found');
+  }
+}).listen(PORT);
+
+module.exports = app;
