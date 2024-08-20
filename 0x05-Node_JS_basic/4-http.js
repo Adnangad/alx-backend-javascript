63eb12c53ce57f8bf6b36e574545f621ebf6edd4
Adnangad <gardobuyaadnan@gmail.com>
2024-08-20 11:19:12 +0300

    4-created a simple server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require("http"),
+    app = http.createServer((req, resp) => {
+        resp.setHeader(
+            "Content-Type",
+            "text/plain"
+        );
+        resp.end("Hello Holberton School!");
+    }),
+    PORT = 1245;
+app.listen(
+    PORT,
+    () => {
+        console.log("...");
+    }
+);
+module.exports = app;
--- 0x05-Node_JS_basic/temp.js
+++
@@ -1,3 +0,0 @@
-pz = 'Hello, world how, are you\n Hii, how';
-x = 1;
-console.log(pz[x].split(','));
