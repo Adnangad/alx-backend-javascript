3d72fa3f4091c97280afa3ce761556bff04d87f4
Adnangad <gardobuyaadnan@gmail.com>
2024-08-20 11:35:41 +0300

    4-created a simple server and fixed errors

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,21 @@
 const http = require("http"),
     app = http.createServer((req, resp) => {
-        resp.setHeader(
-            "Content-Type",
-            "text/plain"
-        );
-        resp.end("Hello Holberton School!");
+        try {
+            resp.statusCode = 200;
+            resp.setHeader(
+                "Content-Type",
+                "text/plain"
+            );
+            resp.end("Hello Holberton School!");
+        } catch (error) {
+            throw new Error(error);
+        }
     }),
     PORT = 1245;
 app.listen(
     PORT,
     () => {
+        console.log("...");
     }
 );
 module.exports = app;
