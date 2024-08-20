81f124739d459c626f1e29a9bd667c5a6e092916
Adnangad <gardobuyaadnan@gmail.com>
2024-08-20 13:55:46 +0300

    6-express app

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,15 +1,15 @@
-const express = require("express"),
-    app = express(),
-    PORT = 1245;
+const express = require('express');
+const app = express();
+const PORT = 1245;
 app.get(
-    "/",
-    (req, resp) => {
-        resp.send("Hello Holberton School!");
-    }
+  '/',
+  (_, resp) => {
+    resp.send('Hello Holberton School!');
+  }
 ).listen(
-    PORT,
-    () => {
-        console.log(`Listening at local host port ${PORT}`);
-    }
-)
+  PORT,
+  () => {
+    console.log(`Listening at local host port ${PORT}`);
+  }
+);
 module.exports = app;
