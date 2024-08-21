e01e0dd88b4f467f66c107a4b3b599da36403a7a
Adnangad <gardobuyaadnan@gmail.com>
2024-08-21 22:56:36 +0300

    0-calc

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,36 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('Testing the calculateNumber function', function () {
-    it('returns 4 when 1 and 3 are passed', function () {
-        assert.strictEqualqual(calculateNumber(1, 3), 4);
-    });
-    it('returns a whole number when passed', function() {
-        assert.strictEqualqual(calculateNumber(1, 3.7), 5);
-    });
-    it('returns a whole number when passed', function() {
-        assert.strictEqualqual(calculateNumber(1.2, 3.7), 5);
-    });
-    it('returns a whole number when passed', function() {
-        assert.strictEqualqual(calculateNumber(1.5, 3.7), 6);
-    });    
-})
+describe('calculateNumber', () => {
+  it('testing whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it('testing fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it('testng rounding down fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
+
+  it('rounding down a', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('rounding up b', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding up a and b', () => {
+    assert.strictEqual(calculateNumber(3.6, 2.5), 7);
+  });
+
+  it('rounding up a', () => {
+    assert.strictEqual(calculateNumber(3.6, 2.0), 6);
+  });
+
+  it('rounding down a and b', () => {
+    assert.strictEqual(calculateNumber(2.49, 3.499), 6);
+  });
+});
