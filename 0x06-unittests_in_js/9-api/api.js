63fd97324c915ee68406cfa8f3c265aea1acfd2c
Adnangad <gardobuyaadnan@gmail.com>
2024-08-22 15:48:28 +0300

    9

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 7865;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+})
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const cart_id = req.params.id;
+    res.send(`Payment methods for cart :${cart_id}`);
+})
+app.listen(PORT, () => {
+    console.log(`API available on localhost port ${PORT}`);
+})
+module.exports = app;
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const expect = chai.expect;
+const request = require("request");
+const app = require("./api");
+
+describe("It tests the app", function () {
+  const url = "http://localhost:7865";
+  it("tests the response status", function (done) {
+    request.get(`${url}/cart/12`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal("Payment methods for cart :12");
+      done();
+    });
+  });
+  it("tests the response status of an invalid id", function (done) {
+    request.get(`${url}/cart/hello`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
