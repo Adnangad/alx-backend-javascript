ab57afd5afc9b6bb6da1a55f9bf60e2b763ae9f9
Adnangad <gardobuyaadnan@gmail.com>
2024-08-22 12:37:01 +0300

    5-console spy

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,26 +1,25 @@
 const chai = require('chai');
-const expect  = chai.expect;
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./5-payment')
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./5-payment');
 const sinon = require('sinon');
 
 describe('Tests sendPaymentRequestApi module', function () {
-    let logspy;
+  let logspy;
 
-    before(function() {
-        logspy = sinon.spy(console, 'log')
-    });
-    afterEach(function() {
-        sinon.reset()
-    });
-    it('checks if results 1 were logged as expected', function () {
-        sendPaymentRequestToApi(100, 20);
-        expect(logspy.calledOnce).to.be.true;
-        expect(logspy.calledWith('The total is: 120')).to.be.true
-    });
-    it('checks if the results were logged as expected', function() {
-        sendPaymentRequestToApi(10, 10);
-        expect(logspy.calledOnce).to.be.true;
-        expect(logspy.calledWith('The total is: 20')).to.be.true
-    })
+  before(function () {
+    logspy = sinon.spy(console, 'log');
+  });
+  afterEach(function () {
+    sinon.reset();
+  });
+  it('checks if results 1 were logged as expected', function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(logspy.calledOnce).to.be.true;
+    expect(logspy.calledWith('The total is: 120')).to.be.true;
+  });
+  it('checks if the results were logged as expected', function () {
+    sendPaymentRequestToApi(10, 10);
+    expect(logspy.calledOnce).to.be.true;
+    expect(logspy.calledWith('The total is: 20')).to.be.true;
+  });
 });
